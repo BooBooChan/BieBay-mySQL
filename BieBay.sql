7a159650d6b541b1271ef22ed2ee9af3fc9f778b
Bridget Bourne <bbourne8891@hotmail.com>
2017-06-25 12:23:13 -0400

    SQL file practically set to go

---
+++ BieBay.sql
@@ -0,0 +1,39 @@
+#Be ready to load a pre-existing biebay_database...
+DROP DATABASE IF EXISTS biebay_database;
+#...otherwise, create a new one.
+CREATE DATABASE biebay_database;
+#Prepare database to be executed by the Terminal using USE DATABASE
+USE DATABASE biebay_database;
+#Database needs data, so CREATE a TABLE.
+CREATE TABLE products (
+#Now start adding columns
+`item_id` INT(1) AUTO_INCREMENT,#AUTO_INCREMENT will assign unique ID number to every new item entered.
+`product_name` VARCHAR(100),#Number in parentheses for VARCHAR determines max characters in entered value.
+`department_name` VARCHAR(50),
+`price_in_us_dollars` INTEGER,
+`stock_quantity` INTEGER,
+`autographed` TINYINT(1),#Seting a TINYINT between 0 and 1 will allow a boolean "true" or "false" statment
+PRIMARY KEY (`item_id`)#Primary key allegedly updates 'item_id' on my behalf for every new item entered. Also, PRIMARY KEY has no NULL value.
+);
+#Then add values to those columns.
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity, autographed)
+VALUES(1, "perfume", "smelly things", 50, 10, 0);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity, autographed)
+VALUES(2, "deluxe album", "noisy things", 80, 40, 1);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity, autographed)
+VALUES(3, "baseball cap", "itchy things", 20, 100, 0);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity, autographed)
+VALUES(4, "baseball", "lopsided things", 100, 10, 1);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity, autographed)
+VALUES(5, "cardboard cutout", "flat things", 25, 150, 1);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity, autographed)
+VALUES(6, "tattoo", "sharp and pointy things", 80, 40, 1);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity, autographed)
+VALUES(7, "concert vhs tape", "nonexistent things", 100, 1000, 1);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity, autographed)
+VALUES(8, "star bought and named by Justin Bieber", "out-of-reach things", 1, 15000, 1);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity, autographed)
+VALUES(9, "action figure", "plastic things", 15, 75, 0);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity, autographed)
+VALUES(10, "makeup", "runny things", 200, 200, 0);
+	
---
+++ bieBayCustomer.js
@@ -0,0 +1,19 @@
+//First, summon the npm packages and mySQL.
+
+//Remember, you need to console.log everything that needs to print for everyone to see.
+//Also, videorecord a demo of your app.
+var inquirer = require(inquirer);
+var takeYouToSQL = require(mySQL);
+var bieBayDatabase = require(/*bieBay database from mySQL*/);//Look up how to pull specific databases from mySQL to this JS sheet.
+//Next, 
+function ItemsAvail(id, name, price){
+	itemsAvail.id = ___;
+	itemsAvail.name = ___;
+	itemsAvail.price = ___;
+
+};
+var onThisStretchOfRoadTenYearsAgoISawTheWorstAccidentIEverSeen = __;
+
+	console.log(bieBay.products.item_id);
+	console.log(biebay.products.product_name);
+	console.log(bieBay.products.price);
